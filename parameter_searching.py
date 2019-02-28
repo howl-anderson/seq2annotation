22659f43a74fc91550760657cc0673ee8f51f7d4
Xiaoquan Kong <u1mail2me@gmail.com>
2019-02-28 15:29:10 +0800

    Add hyper-parameter searching function

---
+++ parameter_searching.py
@@ -0,0 +1,36 @@
+import ray
+import ray.tune as tune
+from seq2annotation.trainer.train_model import train_model
+from seq2annotation.algorithms.BiLSTM_CRF_model import BilstmCrfModel
+
+ray.init()
+
+
+def train_func(config, reporter):
+    evaluate_result, export_results = train_model(
+        data_dir='./data', result_dir='./results',
+        train_spec={'max_steps': 150000},
+        hook={
+            'stop_if_no_increase': {
+                'min_steps': 100,
+                'run_every_secs': 60,
+                'max_steps_without_increase': 10000
+            }
+        },
+        model=BilstmCrfModel,
+        optimizer_params={'learning_rate': config['momentum']},
+        **BilstmCrfModel.default_params(),
+    )
+
+    reporter(mean_accuracy=evaluate_result['correct_rate'])
+
+
+all_trials = tune.run_experiments({
+    "my_experiment": {
+        "run": train_func,
+        "stop": {"mean_accuracy": 0.96},
+        "config": {"momentum": tune.grid_search([0.05, 0.1, 0.2])}
+    }
+})
+
+print(all_trials)

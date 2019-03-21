273cc8c168a0b12699fe52caef3607db457bd984
Xiaoquan Kong <u1mail2me@gmail.com>
2019-03-21 20:21:36 +0800

    Add TPU supporting

--- model.py
+++ model.py
@@ -24,3 +24,34 @@ class Model(object):
         )
 
         return evaluate_result, export_results, final_saved_model
+
+
+class TpuModel(object):
+    def __init__(self, arch_setting):
+        pass
+
+    def train_and_eval_then_save(self, train_inpf, eval_inpf, saved_model_dir):
+        evaluate_result, export_results, final_saved_model = train_model(
+            train_inpf=train_inpf,
+            eval_inpf=eval_inpf,
+            forced_saved_model_dir=saved_model_dir,
+            data_dir='./data', result_dir='gs://ecarx/results',
+            train_spec={'max_steps': 150000},
+            use_tpu=True,
+            batch_size=1024,
+            hook={
+                'stop_if_no_increase': {
+                    'min_steps': 100,
+                    'run_every_secs': 60,
+                    'max_steps_without_increase': 10000
+                }
+            },
+            tpu_config={
+                'tpu_name': 'u1mail2me',
+                'zone': None,
+                'gcp_project': None
+            },
+            model=BilstmCrfModel, **BilstmCrfModel.default_params()
+        )
+
+        return evaluate_result, export_results, final_saved_model
---
+++ tpu_task_main.py
@@ -0,0 +1,72 @@
+from corpus import Corpus
+from task_status import TaskStatus
+from model_saver import ModelSaver
+from performance_metrics import PerformanceMetrics
+
+from utils import read_configure, build_input_func, build_gold_generator_func, generator_func
+from model import TpuModel
+
+config = read_configure()
+
+task_status = TaskStatus(config)
+
+# read data according configure
+corpus = Corpus(config)
+corpus.prepare()
+train_data_generator_func = corpus.get_generator_func(corpus.TRAIN)
+eval_data_generator_func = corpus.get_generator_func(corpus.EVAL)
+
+# build model according configure
+model = TpuModel(config['model']['arch'])
+
+# send START status to monitor system
+task_status.send_status(task_status.START)
+
+# train and evaluate model
+train_input_func = build_input_func(train_data_generator_func, config['model'])
+eval_input_func = build_input_func(eval_data_generator_func, config['model'])
+
+# ***** test ******
+train_iterator = train_input_func()
+import tensorflow as tf
+import sys
+
+# data_generator = generator_func(train_data_generator_func)
+# for i, data in enumerate(data_generator):
+#     print(i, data)
+#
+# sys.exit(0)
+
+# with tf.Session() as sess:
+#     sess.run(tf.tables_initializer())
+#
+#     counter = 0
+#     while True:
+#         try:
+#             value = sess.run(train_iterator[0]['words'])
+#             counter += 1
+#             print(value)
+#             break
+#         except tf.errors.OutOfRangeError:
+#             break
+#
+# print(counter)
+# #
+# sys.exit(0)
+# ***** /test ******
+
+evaluate_result, export_results, final_saved_model = model.train_and_eval_then_save(
+    train_input_func,
+    eval_input_func,
+    './results/saved_model'
+)
+
+task_status.send_status(task_status.DONE)
+
+if evaluate_result:
+    performance_metrics = PerformanceMetrics(config)
+    performance_metrics.set_metrics('test_loss', evaluate_result['loss'])
+    performance_metrics.set_metrics('test_acc', evaluate_result['acc'])
+
+model_saver = ModelSaver(config)
+model_saver.save_model(final_saved_model)

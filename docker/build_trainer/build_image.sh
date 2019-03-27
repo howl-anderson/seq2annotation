7cae3007abeef2264350abd985b8a108460b1273
Xiaoquan Kong <u1mail2me@gmail.com>
2019-03-27 15:08:42 +0800

    Add trainer building files

---
+++ docker/build_trainer/build_image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build --force-rm --tag ner_trainer:0.0.1 .
---
+++ docker/build_trainer/run_image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -v /path/to/configure.json:/data/configure.json ner_trainer:0.0.1

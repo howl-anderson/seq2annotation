d05958757ababb915ddcd0e72fdf9f572ddc108a
Xiaoquan Kong <u1mail2me@gmail.com>
2019-04-23 20:37:58 +0800

    Bump version: 0.1.2 → 0.2.0

--- seq2annotation/__init__.py
+++ seq2annotation/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 from seq2annotation.trainer.train_model import train_model
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='seq2annotation',
-    version='0.1.2',
+    version='0.2.0',
     packages=setuptools.find_packages(),
     include_package_data=True,
     url='',

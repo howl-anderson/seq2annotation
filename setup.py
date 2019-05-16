3fd94a29cd267b1b3ba04f5bc50a9b838490e61f
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-16 14:56:46 +0800

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='seq2annotation',
-    version='0.2.1',
+    version='0.3.0',
     packages=setuptools.find_packages(),
     include_package_data=True,
     url='',

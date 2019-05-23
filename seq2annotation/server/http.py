a818b26b2f5ff7a54bd4d162d76c9cf80b7950e0
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-23 16:07:51 +0800

    Refactor inference service and add batch inference function

--- seq2annotation/server/http.py
+++ seq2annotation/server/http.py
@@ -1,98 +1,23 @@
-import os
 import sys
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from tokenizer_tools.tagset.NER.BILUO import BILUOSequenceEncoderDecoder
-from tokenizer_tools.tagset.offset.sequence import Sequence
 
-decoder = BILUOSequenceEncoderDecoder()
-
+from seq2annotation.server.tensorflow_inference import Inference
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 # app.config['DEBUG'] = True
 CORS(app)
 
-from tensorflow.contrib import predictor
-
 server = None
 
 
 def load_predict_fn(export_dir):
     global server
-    server = Server(export_dir)
+    server = Inference(export_dir)
 
     return server
-
-
-class IndexToStringTable(object):
-    def __init__(self, lookup_file):
-        self.dict = {}
-
-        with open(lookup_file) as fd:
-            for index, string in enumerate(fd):
-                self.dict[index] = string.strip()
-
-    def lookup(self, index):
-        return self.dict[index]
-
-
-class IndexTable(object):
-    def __init__(self, lookup_file):
-        self.dict = {}
-
-        with open(lookup_file) as fd:
-            for index, string in enumerate(fd):
-                string = string.strip()
-                self.dict[string] = index
-
-    def lookup(self, string):
-        return self.dict[string]
-
-
-class Server(object):
-    def __init__(self, model_dir):
-        self.model_dir = model_dir
-        self.predict_fn = predictor.from_saved_model(model_dir)
-
-        # self.char_to_index_table = IndexTable(os.path.join(model_dir, 'assets.extra/vocab.txt'))
-        # self.index_to_tag_table = IndexToStringTable(os.path.join(model_dir, 'assets.extra/tags.txt'))
-
-    def serve(self, input_text, raise_exception=False):
-        # input_text = list(map(self.char_to_index_table.lookup, input_text))
-
-        input_feature = {
-            'words': [[i for i in input_text]],
-            'words_len': [len(input_text)],
-        }
-
-        print(input_feature)
-
-        predictions = self.predict_fn(input_feature)
-        tags = predictions['tags'][0]
-        # print(predictions['tags'])
-
-        # decode Unicode
-        tags_seq = [i.decode() for i in tags]
-
-        print(tags_seq)
-
-        # BILUO to offset
-        failed = False
-        try:
-            seq = decoder.to_offset(tags_seq, input_text)
-        except:
-            if not raise_exception:
-                # invalid tag sequence will raise exception
-                # so return a empty result
-                seq = Sequence(input_text)
-                failed = True
-            else:
-                raise
-        # print(seq)
-
-        return seq, tags_seq, failed
 
 
 @app.route("/parse", methods=['GET'])
@@ -101,16 +26,39 @@ def single_tokenizer():
 
     print(text_msg)
 
-    seq, tags, failed = server.serve(text_msg)
+    raw_input_text, seq, tags, failed = server.infer(text_msg)
 
     print(tags)
     # print(seq)
 
     response = {
-        'text': text_msg,
-        'spans': [{'start': i.start, 'end': i.end, 'type': i.entity} for i in seq.span_set],
+        'text': raw_input_text,
+        'spans': [{'start': i.start, 'end': i.end, 'type': i.entity} for i in
+                  seq.span_set],
         'ents': list({i.entity.lower() for i in seq.span_set})
     }
+
+    return jsonify(response)
+
+
+@app.route("/parse", methods=['POST'])
+def batch_infer():
+    text_msg = request.get_json()
+
+    print(text_msg)
+
+    infer_results = server.batch_infer(text_msg)
+
+    response = []
+    for raw_input_text, seq, tags, failed in infer_results:
+        response.append(
+            {
+                'text': raw_input_text,
+                'spans': [{'start': i.start, 'end': i.end, 'type': i.entity}
+                          for i in seq.span_set],
+                'ents': list({i.entity.lower() for i in seq.span_set})
+            }
+        )
 
     return jsonify(response)
 
---
+++ seq2annotation/server/tensorflow_inference.py
@@ -0,0 +1,65 @@
+import keras
+
+from tokenizer_tools.tagset.NER.BILUO import BILUOSequenceEncoderDecoder
+from tokenizer_tools.tagset.offset.sequence import Sequence
+from tensorflow.contrib import predictor
+
+decoder = BILUOSequenceEncoderDecoder()
+
+
+class Inference(object):
+    def __init__(self, model_path):
+        # load model
+        self.model_dir = model_path
+        self.predict_fn = predictor.from_saved_model(model_path)
+
+    def infer(self, input_text):
+        infer_result = self._infer(input_text)
+        return infer_result[0]
+
+    def batch_infer(self, input_text):
+        return self._infer(input_text)
+
+    def _infer(self, input_text):
+        if isinstance(input_text, str):
+            input_list = [input_text]
+        else:
+            input_list = input_text
+
+        raw_sequences = [[i for i in text] for text in input_list]
+
+        sentence = keras.preprocessing.sequence.pad_sequences(
+            raw_sequences, dtype='object',
+            padding='post', truncating='post', value=['<pad>']
+        ).tolist()
+
+        input_feature = {
+            'words': [[i for i in text] for text in sentence],
+            'words_len': [len(text) for text in sentence],
+        }
+
+        print(input_feature)
+
+        predictions = self.predict_fn(input_feature)
+        tags_list = predictions['tags']
+
+        infer_result = []
+        for raw_input_text, tags in zip(input_list, tags_list):
+            # decode Unicode
+            tags_seq = [i.decode() for i in tags]
+
+            print(tags_seq)
+
+            # BILUO to offset
+            failed = False
+            try:
+                seq = decoder.to_offset(tags_seq, input_text)
+            except:
+                # invalid tag sequence will raise exception
+                # so return a empty result
+                seq = Sequence(input_text)
+                failed = True
+
+            infer_result.append((raw_input_text, seq, tags_seq, failed))
+
+        return infer_result

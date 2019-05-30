00277f2e899b15c06dd3c3dcf37aa5016d68bce0
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-30 21:51:29 +0800

    Force clean docker cache

--- docker/build_base_image/build_image.sh
+++ docker/build_base_image/build_image.sh
@@ -3,4 +3,4 @@
 arch=${1:-cpu}
 version=${2:-0.0.1}
 
-docker build --force-rm -f ./dockerfiles/${arch}.Dockerfile --tag ner_base-${arch}:${version} .
+docker build --no-cache --force-rm -f ./dockerfiles/${arch}.Dockerfile --tag ner_base-${arch}:${version} .
--- docker/build_trainer/build_image.sh
+++ docker/build_trainer/build_image.sh
@@ -3,4 +3,4 @@
 base_image=${1:-ner_base-cpu}
 tag=${2:-0.0.1}
 
-docker build --force-rm --build-arg BASE_IMAGE_NAME=${base_image} --build-arg BASE_IMAGE_TAG=${tag} --tag ner_trainer:0.0.1 .
+docker build --no-cache --force-rm --build-arg BASE_IMAGE_NAME=${base_image} --build-arg BASE_IMAGE_TAG=${tag} --tag ner_trainer:0.0.1 .

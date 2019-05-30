0d56d22790369613ae154c04ec21cbdfe981f2d2
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-30 21:44:15 +0800

    Update

--- docker/local_build_all.bash
+++ docker/local_build_all.bash
@@ -1,2 +1,3 @@
-(cd ./build_base_image && bash build_image.sh)
-(cd ./build_trainer && bash build_image.sh)
+(cd ./build_base_image && docker rmi -f ner_base-cpu:0.0.1 && bash build_image.sh)
+(cd ./build_trainer && docker rmi -f ner_trainer:0.0.1 && bash build_image.sh)
+(cd ./build_server && docker rmi -f ner:0.0.1 && bash build_image.sh)

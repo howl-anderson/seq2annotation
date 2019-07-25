f7518d1ab888b281343a0b7eb5e580b32e5d7c8f
Xiaoquan Kong <u1mail2me@gmail.com>
2019-07-25 09:32:18 +0800

    integrate with ioflow

--- seq2annotation/trainer/cli.py
+++ seq2annotation/trainer/cli.py
@@ -1,5 +1,7 @@
+import json
+
 from ioflow.corpus import get_corpus_processor
-from ioflow.performance_reporter import get_performance_reporter
+from ioflow.eval_reporter import get_eval_reporter
 from ioflow.task_status import get_task_status
 from ioflow.model_saver import get_model_saver
 from ioflow.configure import read_configure
@@ -19,7 +21,12 @@ config.update(raw_config)
 
 task_status = get_task_status(config)
 
+task_status.send_status(task_status.START)
+
 # read data according configure
+# # report status: start to download corpus
+task_status.send_status(task_status.START_DOWNLOAD_CORPUS)
+
 corpus = get_corpus_processor(config)
 corpus.prepare()
 train_data_generator_func = corpus.get_generator_func(corpus.TRAIN)
@@ -36,8 +43,8 @@ config['tags_data'] = generate_tagset(corpus_meta_data['tags'])
 # build model according configure
 
 
-# send START status to monitor system
-task_status.send_status(task_status.START)
+# # report status: start to process corpus
+task_status.send_status(task_status.START_PROCESS_CORPUS)
 
 # train and evaluate model
 train_input_func = build_input_func(train_data_generator_func, config)
@@ -70,18 +77,43 @@ import tensorflow as tf
 # sys.exit(0)
 # ***** /test ******
 
+# # report status: start to train
+task_status.send_status(task_status.START_TRAIN)
+task_status.send_progress(0)
+task_status.send_progress(50)
+
 evaluate_result, export_results, final_saved_model = model.train_and_eval_then_save(
     train_input_func,
     eval_input_func,
     config
 )
 
-task_status.send_status(task_status.DONE)
+task_status.send_progress(100)
 
-if evaluate_result:
-    performance_metrics = get_performance_reporter(config)
-    performance_metrics.log_performance('test_loss', evaluate_result['loss'])
-    performance_metrics.log_performance('test_acc', evaluate_result['acc'])
+# # report status: start to test
+task_status.send_status(task_status.START_TEST)
+
+from seq2annotation.server.tensorflow_inference import Inference
+eval_reporter = get_eval_reporter(config)
+
+# result_list = []
+inference = Inference(final_saved_model)
+for item in eval_data_generator_func():
+    result = inference.infer(item.text)
+    eval_reporter.record_x_and_y(item, json.dumps(result))
+    # result_list.append(result)
+
+eval_reporter.submit()
+
+# if evaluate_result:
+#     performance_metrics = get_performance_reporter(config)
+#     performance_metrics.log_performance('test_loss', evaluate_result['loss'])
+#     performance_metrics.log_performance('test_acc', evaluate_result['acc'])
+
+# # report status: start to upload model
+task_status.send_status(task_status.START_UPLOAD_MODEL)
 
 model_saver = get_model_saver(config)
 model_saver.save_model(final_saved_model)
+
+task_status.send_status(task_status.DONE)

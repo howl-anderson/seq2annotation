05286f6f03548ee6e0886407313733f72d83636f
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-03 15:53:10 +0800

    Code clean and reformat

--- seq2annotation/algorithms/model.py
+++ seq2annotation/algorithms/model.py
@@ -27,26 +27,26 @@ class Model(object):
         self.params = params
 
     def tpu_input_layer(self):
-        word_ids = self.features['words']
+        word_ids = self.features["words"]
 
-        nwords = tf.identity(self.features['words_len'], name='input_words_len')
+        nwords = tf.identity(self.features["words_len"], name="input_words_len")
 
-        indices = self.params['_indices']
-        num_tags = self.params['_num_tags']
+        indices = self.params["_indices"]
+        num_tags = self.params["_num_tags"]
 
         return indices, num_tags, word_ids, nwords
 
     def input_layer(self):
         # data = np.loadtxt(self.params['vocab'], dtype=np.unicode, encoding=None)
-        data = self.params['vocab_data']
+        data = self.params["vocab_data"]
         mapping_strings = tf.Variable(data)
         vocab_words = tf.contrib.lookup.index_table_from_tensor(
-            mapping_strings, num_oov_buckets=1)
+            mapping_strings, num_oov_buckets=1
+        )
 
         # Word Embeddings
-        words = tf.identity(self.features['words'], name='input_words')
+        words = tf.identity(self.features["words"], name="input_words")
         word_ids = vocab_words.lookup(words)
-
 
         #
         # raw_nwords = tf.identity(features['words_len'], name='input_words_len')
@@ -60,11 +60,15 @@ class Model(object):
         # nwords = features['words_len']
         # nwords = tf.convert_to_tensor(nwords)
 
-        nwords = tf.identity(self.features['words_len'], name='input_words_len')
+        nwords = tf.identity(self.features["words_len"], name="input_words_len")
 
         # get tag info
         # with Path(self.params['tags']).open() as f:
-        indices = [idx for idx, tag in enumerate(self.params['tags_data']) if tag.strip() != 'O']
+        indices = [
+            idx
+            for idx, tag in enumerate(self.params["tags_data"])
+            if tag.strip() != "O"
+        ]
         num_tags = len(indices) + 1
 
         # # true tags to ids
@@ -80,19 +84,24 @@ class Model(object):
         # glove = np.load(params['glove'])['embeddings']  # np.array
 
         # training the embedding during training
-        glove = np.zeros((self.params['embedding_vocabulary_size'], self.params['embedding_dim']), dtype=np.float32)
+        glove = np.zeros(
+            (self.params["embedding_vocabulary_size"], self.params["embedding_dim"]),
+            dtype=np.float32,
+        )
 
         # Add OOV word embedding
-        embedding_array = np.vstack([glove, [[0.] * self.params['embedding_dim']]])
+        embedding_array = np.vstack([glove, [[0.0] * self.params["embedding_dim"]]])
 
-        embedding_variable = tf.Variable(embedding_array, dtype=tf.float32, trainable=True)
+        embedding_variable = tf.Variable(
+            embedding_array, dtype=tf.float32, trainable=True
+        )
         embeddings = tf.nn.embedding_lookup(embedding_variable, word_ids)
 
         return embeddings
 
     def dropout_layer(self, data):
-        training = (self.mode == tf.estimator.ModeKeys.TRAIN)
-        output = tf.layers.dropout(data, rate=self.params['dropout'], training=training)
+        training = self.mode == tf.estimator.ModeKeys.TRAIN
+        output = tf.layers.dropout(data, rate=self.params["dropout"], training=training)
 
         return output
 
@@ -103,20 +112,22 @@ class Model(object):
 
     def load_tag_data(self):
         # data = np.loadtxt(self.params['tags'], dtype=np.unicode, encoding=None)
-        data = self.params['tags_data']
+        data = self.params["tags_data"]
         mapping_strings = tf.Variable(data)
 
         return mapping_strings
 
     def load_word_data(self):
-        data = np.loadtxt(self.params['words'], dtype=np.unicode, encoding=None)
+        data = np.loadtxt(self.params["words"], dtype=np.unicode, encoding=None)
         mapping_strings = tf.Variable(data.reshape((-1,)))
 
         return mapping_strings
 
     def tag2id(self, labels, name=None):
         mapping_strings = self.load_tag_data()
-        vocab_tags = tf.contrib.lookup.index_table_from_tensor(mapping_strings, name=name)
+        vocab_tags = tf.contrib.lookup.index_table_from_tensor(
+            mapping_strings, name=name
+        )
 
         tags = vocab_tags.lookup(labels)
 
@@ -125,7 +136,8 @@ class Model(object):
     def id2tag(self, pred_ids, name=None):
         mapping_strings = self.load_tag_data()
         reverse_vocab_tags = tf.contrib.lookup.index_to_string_table_from_tensor(
-            mapping_strings, name=name)
+            mapping_strings, name=name
+        )
 
         pred_strings = reverse_vocab_tags.lookup(tf.to_int64(pred_ids))
 
@@ -134,7 +146,8 @@ class Model(object):
     def id2word(self, word_ids, name=None):
         mapping_strings = self.load_word_data()
         reverse_vocab_tags = tf.contrib.lookup.index_to_string_table_from_tensor(
-            mapping_strings, name=name)
+            mapping_strings, name=name
+        )
 
         word_strings = reverse_vocab_tags.lookup(tf.to_int64(word_ids))
 
@@ -143,7 +156,8 @@ class Model(object):
     def loss_layer(self, preds, ground_true, nwords, crf_params):
         with tf.name_scope("CRF_log_likelihood"):
             log_likelihood, _ = tf.contrib.crf.crf_log_likelihood(
-                preds, ground_true, nwords, crf_params)
+                preds, ground_true, nwords, crf_params
+            )
 
         loss = tf.reduce_mean(-log_likelihood)
 
@@ -151,8 +165,7 @@ class Model(object):
 
     def crf_decode_layer(self, logits, crf_params, nwords):
         with tf.name_scope("CRF_decode"):
-            pred_ids, _ = tf.contrib.crf.crf_decode(logits, crf_params,
-                                                        nwords)
+            pred_ids, _ = tf.contrib.crf.crf_decode(logits, crf_params, nwords)
 
         return pred_ids
 
@@ -163,12 +176,11 @@ class Model(object):
         # metrics_correct_rate = correct_rate(tags, pred_ids, weights)
 
         metrics = {
-            'acc': tf.metrics.accuracy(tags, pred_ids, weights),
-            'precision': precision(tags, pred_ids, num_tags, indices,
-                                   weights),
-            'recall': recall(tags, pred_ids, num_tags, indices, weights),
-            'f1': f1(tags, pred_ids, num_tags, indices, weights),
-            'correct_rate': correct_rate(tags, pred_ids, weights),
+            "acc": tf.metrics.accuracy(tags, pred_ids, weights),
+            "precision": precision(tags, pred_ids, num_tags, indices, weights),
+            "recall": recall(tags, pred_ids, num_tags, indices, weights),
+            "f1": f1(tags, pred_ids, num_tags, indices, weights),
+            "correct_rate": correct_rate(tags, pred_ids, weights),
             # 'golden': (golden, tf.zeros([], tf.int32)),
             # 'predict': (predict, tf.zeros([], tf.int32))
         }
@@ -192,48 +204,52 @@ class Model(object):
         data = self.dropout_layer(data)
         logits = self.dense_layer(data, num_tags)
 
-        crf_params = tf.get_variable("crf", [num_tags, num_tags],
-                                     dtype=tf.float32)
+        crf_params = tf.get_variable("crf", [num_tags, num_tags], dtype=tf.float32)
 
         pred_ids = self.crf_decode_layer(logits, crf_params, nwords)
-        pred_strings = self.id2tag(pred_ids, name='predict')
+        pred_strings = self.id2tag(pred_ids, name="predict")
 
         # word_strings = self.id2word(word_ids, name='word_strings')
 
         # print(word_strings)
 
         if self.mode == tf.estimator.ModeKeys.PREDICT:
-            predictions = {
-                'pred_ids': pred_ids,
-                'tags': pred_strings
-            }
+            predictions = {"pred_ids": pred_ids, "tags": pred_strings}
 
-            if self.params['use_tpu']:
+            if self.params["use_tpu"]:
                 return tf.contrib.tpu.TPUEstimatorSpec(
                     self.mode, predictions=predictions
                 )
             else:
-                return tf.estimator.EstimatorSpec(self.mode,
-                                                  predictions=predictions)
+                return tf.estimator.EstimatorSpec(self.mode, predictions=predictions)
         else:
             # true_tag_ids = self.labels
-            true_tag_ids = self.tag2id(self.labels, 'labels')
+            true_tag_ids = self.tag2id(self.labels, "labels")
 
             # print(pred_strings)
             # print(self.labels)
 
             loss = self.loss_layer(logits, true_tag_ids, nwords, crf_params)
 
-            metrics = self.compute_metrics(true_tag_ids, pred_ids,
-                                           num_tags, indices, nwords)
+            metrics = self.compute_metrics(
+                true_tag_ids, pred_ids, num_tags, indices, nwords
+            )
 
             if self.mode == tf.estimator.ModeKeys.EVAL:
-                if self.params['use_tpu']:
+                if self.params["use_tpu"]:
+
                     def my_metric_fn(true_tag_ids, pred_ids, num_tags, indices, nwords):
-                        return self.compute_metrics(true_tag_ids, pred_ids, num_tags, indices, nwords)
+                        return self.compute_metrics(
+                            true_tag_ids, pred_ids, num_tags, indices, nwords
+                        )
 
                     return tf.contrib.tpu.TPUEstimatorSpec(
-                        self.mode, loss=loss, eval_metrics=(my_metric_fn, [true_tag_ids, pred_ids, num_tags, indices, nwords])
+                        self.mode,
+                        loss=loss,
+                        eval_metrics=(
+                            my_metric_fn,
+                            [true_tag_ids, pred_ids, num_tags, indices, nwords],
+                        ),
                     )
                 else:
                     # set up metrics reporter
@@ -247,15 +263,17 @@ class Model(object):
                     #     self.mode, loss=loss, eval_metric_ops=metrics, evaluation_hooks=[hook])
 
                     return tf.estimator.EstimatorSpec(
-                        self.mode, loss=loss, eval_metric_ops=metrics)
+                        self.mode, loss=loss, eval_metric_ops=metrics
+                    )
 
             elif self.mode == tf.estimator.ModeKeys.TRAIN:
-                train_op = tf.train.AdamOptimizer(**self.params.get('optimizer_params', {})).minimize(
-                    loss, global_step=tf.train.get_or_create_global_step())
-                if self.params['use_tpu']:
+                train_op = tf.train.AdamOptimizer(
+                    **self.params.get("optimizer_params", {})
+                ).minimize(loss, global_step=tf.train.get_or_create_global_step())
+                if self.params["use_tpu"]:
                     train_op = tf.contrib.tpu.CrossShardOptimizer(train_op)
 
-                if self.params['use_tpu']:
+                if self.params["use_tpu"]:
                     return tf.contrib.tpu.TPUEstimatorSpec(
                         self.mode, loss=loss, train_op=train_op
                     )
@@ -264,9 +282,10 @@ class Model(object):
                     # from ioflow.hooks.metrics_reporter import metrics_report_hook
                     # hook_class = metrics_report_hook(self.params)
                     # hook = hook_class({"train_loss": loss})
-                    
+
                     # return tf.estimator.EstimatorSpec(
                     #     self.mode, loss=loss, train_op=train_op, training_hooks=[hook])
 
                     return tf.estimator.EstimatorSpec(
-                        self.mode, loss=loss, train_op=train_op)
+                        self.mode, loss=loss, train_op=train_op
+                    )
--- seq2annotation/trainer/cli.py
+++ seq2annotation/trainer/cli.py
@@ -1,5 +1,3 @@
-import json
-
 from ioflow.corpus import get_corpus_processor
 from ioflow.eval_reporter import get_eval_reporter
 from ioflow.task_status import get_task_status
@@ -8,7 +6,9 @@ from ioflow.configure import read_configure
 
 from seq2annotation.input import build_input_func, generate_tagset
 from seq2annotation.model import Model
-from seq2annotation.health_check_transponder import run_health_check_transponder_in_background
+from seq2annotation.health_check_transponder import (
+    run_health_check_transponder_in_background,
+)
 
 # start health check
 run_health_check_transponder_in_background()
@@ -34,11 +34,7 @@ eval_data_generator_func = corpus.get_generator_func(corpus.EVAL)
 
 corpus_meta_data = corpus.get_meta_info()
 
-# config['vocab_data'] = corpus_meta_data['vocab']
-# vocab_data_file = pkg_resources.resource_filename(__name__, '../data/unicode_char_list.txt')
-# config['vocab_data'] = np.loadtxt(vocab_data_file, dtype=np.unicode, encoding=None)
-
-config['tags_data'] = generate_tagset(corpus_meta_data['tags'])
+config["tags_data"] = generate_tagset(corpus_meta_data["tags"])
 
 # build model according configure
 
@@ -48,34 +44,11 @@ task_status.send_status(task_status.START_PROCESS_CORPUS)
 
 # train and evaluate model
 train_input_func = build_input_func(train_data_generator_func, config)
-eval_input_func = build_input_func(eval_data_generator_func, config) if eval_data_generator_func else None
-
-# ***** test ******
-# train_iterator = train_input_func()
-import tensorflow as tf
-# import sys
-
-# for i, data in enumerate(train_data_generator_func()):
-#     print(i, data)
-#
-#
-# with tf.Session() as sess:
-#     sess.run(tf.tables_initializer())
-#
-#     counter = 0
-#     while True:
-#         try:
-#             value = sess.run(train_iterator)
-#             counter += 1
-#             print(value)
-#             break
-#         except tf.errors.OutOfRangeError:
-#             break
-#
-# print(counter)
-# #
-# sys.exit(0)
-# ***** /test ******
+eval_input_func = (
+    build_input_func(eval_data_generator_func, config)
+    if eval_data_generator_func
+    else None
+)
 
 # # report status: start to train
 task_status.send_status(task_status.START_TRAIN)
@@ -83,9 +56,7 @@ task_status.send_progress(0)
 task_status.send_progress(50)
 
 evaluate_result, export_results, final_saved_model = model.train_and_eval_then_save(
-    train_input_func,
-    eval_input_func,
-    config
+    train_input_func, eval_input_func, config
 )
 
 task_status.send_progress(100)
@@ -101,16 +72,11 @@ eval_reporter = get_eval_reporter(config)
 # result_list = []
 inference = Inference(final_saved_model)
 for item in eval_data_generator_func():
-    text, result, _, _ = inference.infer(''.join(item.text))
+    text, result, _, _ = inference.infer("".join(item.text))
     eval_reporter.record_x_and_y(item, sequence_to_response(text, result))
     # result_list.append(result)
 
 eval_reporter.submit()
-
-# if evaluate_result:
-#     performance_metrics = get_performance_reporter(config)
-#     performance_metrics.log_performance('test_loss', evaluate_result['loss'])
-#     performance_metrics.log_performance('test_acc', evaluate_result['acc'])
 
 # # report status: start to upload model
 task_status.send_status(task_status.START_UPLOAD_MODEL)

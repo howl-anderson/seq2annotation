caae79f9dcd52131f0655570c115db55a244e19e
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-12 09:53:55 +0800

    Bump version: 0.5.1 → 0.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='seq2annotation',
-    version='0.5.1',
+    version='0.6.0',
     packages=setuptools.find_packages(),
     include_package_data=True,
     url='https://github.com/howl-anderson/seq2annotation',

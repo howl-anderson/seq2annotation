b4ad61cb07fcd771f69131f3ddfb0107bd51da0c
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-18 14:51:57 +0800

    Bugfix: cache too broad exception; minial code improvement

--- seq2annotation/server/tensorflow_inference.py
+++ seq2annotation/server/tensorflow_inference.py
@@ -1,8 +1,12 @@
+from typing import List
+
 import keras
 
 from tokenizer_tools.tagset.NER.BILUO import BILUOSequenceEncoderDecoder
 from tokenizer_tools.tagset.offset.sequence import Sequence
 from tensorflow.contrib import predictor
+
+from tokenizer_tools.tagset.exceptions import TagSetDecodeError
 
 decoder = BILUOSequenceEncoderDecoder()
 
@@ -13,11 +17,11 @@ class Inference(object):
         self.model_dir = model_path
         self.predict_fn = predictor.from_saved_model(model_path)
 
-    def infer(self, input_text):
+    def infer(self, input_text: str):
         infer_result = self._infer(input_text)
         return infer_result[0]
 
-    def batch_infer(self, input_text):
+    def batch_infer(self, input_text: List[str]):
         return self._infer(input_text)
 
     def _infer(self, input_text):
@@ -33,28 +37,31 @@ class Inference(object):
             padding='post', truncating='post', value=['<pad>']
         ).tolist()
 
+        # TODO: batch infer will cause padding, which will maybe cause decoder to offset bug.
         input_feature = {
             'words': [[i for i in text] for text in sentence],
             'words_len': [len(text) for text in raw_sequences],
         }
 
-        print(input_feature)
+        # print(input_feature)
 
         predictions = self.predict_fn(input_feature)
         tags_list = predictions['tags']
 
         infer_result = []
-        for raw_input_text, tags in zip(input_list, tags_list):
+        for raw_input_text, raw_text, normalized_text, tags in zip(input_list, raw_sequences, sentence, tags_list):
             # decode Unicode
             tags_seq = [i.decode() for i in tags]
 
-            print(tags_seq)
+            # print(tags_seq)
 
             # BILUO to offset
             failed = False
             try:
-                seq = decoder.to_offset(tags_seq, input_text)
-            except:
+                seq = decoder.to_offset(tags_seq, raw_text)
+            except TagSetDecodeError as e:
+                print(e)
+
                 # invalid tag sequence will raise exception
                 # so return a empty result
                 seq = Sequence(input_text)

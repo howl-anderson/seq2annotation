0b474d460cc342fd5fb872c78078d1df4fb3025b
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-24 17:22:24 +0800

    bugfix

--- docker/local_build_all.bash
+++ docker/local_build_all.bash
@@ -8,7 +8,13 @@ base_image=${prefix_tag}ner_base-${arch}:${version}
 (cd ./build_base_image && docker rmi -f ${base_image} && bash build_image.sh ${base_image} ${arch})
 
 trainer_image=${prefix_tag}ner_trainer-${arch}:${version}
-(cd ./build_trainer && docker rmi -f ${trainer_image} && bash build_image.sh ${base_image} ${trainer_image})
+test $? -eq 0 && (cd ./build_trainer && docker rmi -f ${trainer_image} && bash build_image.sh ${base_image} ${trainer_image})
 
 server_image=${prefix_tag}ner-${arch}:${version}
-(cd ./build_server && docker rmi -f ${server_image} && bash build_image.sh ${base_image} ${server_image})
+test $? -eq 0 && (cd ./build_server && docker rmi -f ${server_image} && bash build_image.sh ${base_image} ${server_image})
+
+if test $? -eq 0; then
+    echo "Success!"
+else
+    echo "Failed!"
+fi

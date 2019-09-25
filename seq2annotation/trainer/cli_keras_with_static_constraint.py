b927c46e0e9fedc028938b496b79b8a28210e1c9
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-25 10:08:40 +0800

    Update static constraint keras implementation to keep up with normal keras

--- seq2annotation/trainer/cli_keras_with_static_constraint.py
+++ seq2annotation/trainer/cli_keras_with_static_constraint.py
@@ -1,13 +1,10 @@
-import json
 import os
 from collections import Counter
 
-import numpy as np
+import numpy
 import tensorflow as tf
-from tensorflow.python.keras import models
 from tensorflow.python.keras.layers import Embedding, Bidirectional, LSTM
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras import layers
 
 from ioflow.configure import read_configure
 from ioflow.corpus import get_corpus_processor
@@ -15,9 +12,15 @@ from seq2annotation.input import generate_tagset, Lookuper, \
     index_table_from_file
 from tf_crf_layer.crf_helper import allowed_transitions
 from tf_crf_layer.layer import CRF
-from tf_crf_layer.loss import crf_loss
-from tf_crf_layer.metrics import crf_accuracy
+from tf_crf_layer.loss import crf_loss, ConditionalRandomFieldLoss
+from tf_crf_layer.metrics import crf_accuracy, SequenceCorrectness, SequenceSpanAccuracy, sequence_span_accuracy
 from tokenizer_tools.tagset.converter.offset_to_biluo import offset_to_biluo
+
+# tf.enable_eager_execution()
+
+
+from seq2annotation import unrandom
+
 
 config = read_configure()
 
@@ -35,7 +38,12 @@ eval_data = list(eval_data_generator_func())
 
 tag_lookuper = Lookuper({v: i for i, v in enumerate(tags_data)})
 
-vocab_data_file = os.path.join(os.path.dirname(__file__), '../data/unicode_char_list.txt')
+vocab_data_file = config.get("vocabulary_file")
+
+if not vocab_data_file:
+    # load built in vocabulary file
+    vocab_data_file = os.path.join(os.path.dirname(__file__), '../data/unicode_char_list.txt')
+
 vocabulary_lookuper = index_table_from_file(vocab_data_file)
 
 
@@ -44,8 +52,8 @@ def classification_report(y_true, y_pred, labels):
     Similar to the one in sklearn.metrics,
     reports per classs recall, precision and F1 score
     """
-    y_true = np.asarray(y_true).ravel()
-    y_pred = np.asarray(y_pred).ravel()
+    y_true = numpy.asarray(y_true).ravel()
+    y_pred = numpy.asarray(y_pred).ravel()
     corrects = Counter(yt for yt, yp in zip(y_true, y_pred) if yt == yp)
     y_true_counts = Counter(y_true)
     y_pred_counts = Counter(y_pred)
@@ -73,35 +81,23 @@ def classification_report(y_true, y_pred, labels):
                     sum(report2[2]) / N, N) + '\n')
 
 
-def one_hot(a, num_classes):
-    return np.squeeze(np.eye(num_classes)[a.reshape(-1)])
-
-
-def preprocss(data, maxlen=None, intent_lookup_table=None):
+def preprocss(data):
     raw_x = []
     raw_y = []
-    raw_intent = []
 
     for offset_data in data:
         tags = offset_to_biluo(offset_data)
         words = offset_data.text
-        # label = offset_data.extra_attr['domain']
 
         tag_ids = [tag_lookuper.lookup(i) for i in tags]
         word_ids = [vocabulary_lookuper.lookup(i) for i in words]
 
         raw_x.append(word_ids)
         raw_y.append(tag_ids)
-        # raw_intent.append(label)
 
-    # if not intent_lookup_table:
-    #     raw_intent_set = list(set(raw_intent))
-    #     intent_lookup_table = Lookuper({v: i for i, v in enumerate(raw_intent_set)})
+    maxlen = max(len(s) for s in raw_x)
 
-    # intent_int_list = [intent_lookup_table.lookup(i) for i in raw_intent]
-
-    if not maxlen:
-        maxlen = max(len(s) for s in raw_x)
+    print(">>> maxlen: {}".format(maxlen))
 
     x = tf.keras.preprocessing.sequence.pad_sequences(raw_x, maxlen,
                                                       padding='post')  # right padding
@@ -112,19 +108,15 @@ def preprocss(data, maxlen=None, intent_lookup_table=None):
     y = tf.keras.preprocessing.sequence.pad_sequences(raw_y, maxlen, value=0,
                                                       padding='post')
 
-    # intent_np_array = np.array(intent_int_list)
-    # intent_one_hot = one_hot(intent_np_array, np.max(intent_np_array) + 1)
-    intent_one_hot = None
-
-    return x, intent_one_hot, y, intent_lookup_table
+    return x, y
 
 
-train_x, train_intent, train_y, intent_lookup_table = preprocss(train_data, 25)
-test_x, test_intent, test_y, _ = preprocss(eval_data, 25, intent_lookup_table)
+train_x, train_y = preprocss(train_data)
+test_x, test_y = preprocss(eval_data)
 
-EPOCHS = 10
-EMBED_DIM = 64
-BiRNN_UNITS = 200
+EPOCHS = config['epochs']
+EMBED_DIM = config['embedding_dim']
+BiRNN_UNITS = config['lstm_size']
 
 vacab_size = vocabulary_lookuper.size()
 tag_size = tag_lookuper.size()
@@ -133,30 +125,41 @@ allowed = allowed_transitions("BIOUL", tag_lookuper.inverse_index_table)
 
 model = Sequential()
 model.add(Embedding(vacab_size, EMBED_DIM, mask_zero=True))
-model.add(Bidirectional(LSTM(BiRNN_UNITS // 2, return_sequences=True)))
-model.add(CRF(tag_size, transition_constraint=allowed))
+model.add(Bidirectional(LSTM(BiRNN_UNITS, return_sequences=True)))
+model.add(CRF(tag_size, transition_constraint=allowed, name="crf"))
 
 # print model summary
 model.summary()
 
 callbacks_list = []
 
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=config['summary_log_dir'])
-# callbacks_list.append(tensorboard_callback)
-#
-# checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-#     os.path.join(config['model_dir'], 'cp-{epoch:04d}.ckpt'),
-#     load_weights_on_restart=True,
-#     verbose=1
-# )
-# callbacks_list.append(checkpoint_callback)
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=config['summary_log_dir'])
+callbacks_list.append(tensorboard_callback)
 
-model.compile('adam', loss=crf_loss, metrics=[crf_accuracy])
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    os.path.join(config['model_dir'], 'cp-{epoch:04d}.ckpt'),
+    load_weights_on_restart=True,
+    verbose=1
+)
+callbacks_list.append(checkpoint_callback)
+
+metrics_list = []
+
+metrics_list.append(crf_accuracy)
+metrics_list.append(SequenceCorrectness())
+metrics_list.append(sequence_span_accuracy)
+
+loss_func = ConditionalRandomFieldLoss()
+# loss_func = crf_loss
+
+model.compile('adam', loss={'crf': loss_func}, metrics=metrics_list)
 model.fit(
-    [train_x, train_intent], train_y,
+    train_x, train_y,
     epochs=EPOCHS,
-    validation_data=[[test_x, test_intent], test_y],
+    validation_data=[test_x, test_y],
     callbacks=callbacks_list
 )
 
-# tf.keras.experimental.export_saved_model(model, config['saved_model_dir'])
+# Save the model
+model.save(config['h5_model_file'])
+tf.keras.experimental.export_saved_model(model, config['saved_model_dir'])

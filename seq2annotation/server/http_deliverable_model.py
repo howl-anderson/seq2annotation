e5e678baf3e8d8c82be682f72c849bd21f105f5b
Xiaoquan Kong <u1mail2me@gmail.com>
2019-11-04 20:21:37 +0800

    Add a http GUI

--- seq2annotation/server/http_deliverable_model.py
+++ seq2annotation/server/http_deliverable_model.py
@@ -1,34 +1,36 @@
+import os
 import sys
 from typing import Union, List
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
-import tensorflow as tf
-
 from deliverable_model.builtin.processor.biluo_decode_processor import PredictResult
-from deliverable_model.request import Request
-from deliverable_model.serving import DeliverableModel
+from deliverable_model.serving import SimpleModelInference
 
-app = Flask(__name__)
+current_dir = os.path.dirname(__file__)
+http_root_dir = os.path.join(current_dir, "NLP_server_frontend")
+
+app = Flask(__name__, static_url_path="", static_folder=http_root_dir)
+
 app.config["JSON_AS_ASCII"] = False
 # app.config['DEBUG'] = True
 CORS(app)
 
-server = None  # type: DeliverableModel
+server = None  # type: SimpleModelInference
 
 
 def load_predict_fn(export_dir):
     global server
 
-    server = DeliverableModel.load(export_dir)
+    server = SimpleModelInference(export_dir)
 
     return server
 
 
 def seq_to_http(predict: PredictResult):
     return {
-        "text": predict.sequence.text,
+        "text": "".join(predict.sequence.text),
         "spans": [
             {"start": i.start, "end": i.end, "type": i.entity}
             for i in predict.sequence.span_set
@@ -46,36 +48,35 @@ def compose_http_response(seq_or_seq_list: Union[PredictResult, List[PredictResu
     return jsonify(result)
 
 
+@app.route("/", defaults={"path": "NER.html"})
+def send_static(path):
+    return send_from_directory(http_root_dir, path)
+
+
 @app.route("/parse", methods=["GET"])
 def single_tokenizer():
-    text_msg = request.args.get("q")
+    text_msg = request.args.get("q")  # type: str
 
-    request_obj = Request([[i for i in text_msg]])
+    predict_result = list(server.parse([text_msg]))[0]
 
-    response_obj = server.parse(request_obj)
-
-    return compose_http_response(response_obj.data)
+    return compose_http_response(predict_result)
 
 
 @app.route("/parse", methods=["POST"])
 def batch_infer():
     text_msg = request.get_json()
 
-    request_obj = Request([[j for j in i] for i in text_msg])
+    predict_result_list = list(server.parse(text_msg))
 
-    response_obj = server.parse(request_obj)
-
-    return compose_http_response(response_obj.data)
+    return compose_http_response(predict_result_list)
 
 
 def simple_test():
     text_msg = "今天拉萨的天气。"
 
-    request_obj = Request([[i for i in text_msg]])
+    predict_result = list(server.parse([text_msg]))[0]
 
-    response_obj = server.parse(request_obj)
-
-    print(response_obj.data)
+    print(predict_result)
 
 
 if __name__ == "__main__":

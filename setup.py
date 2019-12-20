faeec18f16b73a28dc323572f416341b2c1ac7ed
Xiaoquan Kong <u1mail2me@gmail.com>
2019-12-20 16:46:26 +0800

    Bump version: 0.8.4 → 0.9.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 setup(
     name=os.getenv("_PKG_NAME", "seq2annotation"),  # _PKG_NAME will be used in Makefile for dev release
-    version="0.8.4",
+    version="0.9.0",
     packages=setuptools.find_packages(),
     include_package_data=True,
     url="https://github.com/howl-anderson/seq2annotation",

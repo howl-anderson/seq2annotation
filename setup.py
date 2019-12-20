8456ab646f253974dfe81efa13a3d3f5a3a1b2d3
Xiaoquan Kong <u1mail2me@gmail.com>
2019-12-20 16:09:50 +0800

    Bump version: 0.8.3 → 0.8.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name="seq2annotation",
-    version="0.8.3",
+    version="0.8.4",
     packages=setuptools.find_packages(),
     include_package_data=True,
     url="https://github.com/howl-anderson/seq2annotation",

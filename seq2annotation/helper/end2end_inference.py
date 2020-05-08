35461ed50e35d38df1076337e1887dbaea69d60e
Xiaoquan Kong <u1mail2me@gmail.com>
2020-05-08 19:03:46 +0800

    refactor: group code to function

--- seq2annotation/helper/end2end_inference.py
+++ seq2annotation/helper/end2end_inference.py
@@ -2,38 +2,44 @@ import sys
 
 import deliverable_model.serving as dm
 from tokenizer_tools.tagset.offset.corpus import Corpus
-from tokenizer_tools.tagset.offset.document import Document
 
-# TODO: better CLI interface for end users
-model_dir = sys.argv[1]
-gold_corpus_file = sys.argv[2]
-predicted_corpus_file = sys.argv[3]
 
-gold_corpus = Corpus.read_from_file(gold_corpus_file)
+def main(model_dir, gold_corpus_file, predicted_corpus_file):
+    gold_corpus = Corpus.read_from_file(gold_corpus_file)
 
-dm_model = dm.load(model_dir)
+    dm_model = dm.load(model_dir)
 
-docs = []
-docs_failed = []
+    docs = []
+    docs_failed = []
 
-for gold_doc in gold_corpus:
-    text = gold_doc.text
-    id_ = gold_doc.id
+    for gold_doc in gold_corpus:
+        text = gold_doc.text
+        id_ = gold_doc.id
 
-    request = dm.make_request(query=[text])
-    response = dm_model.inference(request)
-    result = response.data[0]
+        request = dm.make_request(query=[text])
+        response = dm_model.inference(request)
+        result = response.data[0]
 
-    doc = result.sequence
-    doc.id = id_
+        doc = result.sequence
+        doc.id = id_
 
-    if result.is_failed:
-        doc.extra_attr["is_failed"] = True
-        doc.extra_attr["exec_msg"] = result.exec_msg
-        docs_failed.append(doc)
-    else:
-        docs.append(doc )
+        if result.is_failed:
+            doc.extra_attr["is_failed"] = True
+            doc.extra_attr["exec_msg"] = result.exec_msg
+            docs_failed.append(doc)
+        else:
+            docs.append(doc)
 
-Corpus(docs + docs_failed).write_to_file(predicted_corpus_file)
+    predicted_corpus = Corpus(docs + docs_failed)
+    predicted_corpus.write_to_file(predicted_corpus_file)
 
-print(len(docs_failed), len(docs))
+    print(len(docs_failed), len(docs))
+
+
+if __name__ == "__main__":
+    # TODO: better CLI interface for end users
+    model_dir = sys.argv[1]
+    gold_corpus_file = sys.argv[2]
+    predicted_corpus_file = sys.argv[3]
+
+    main(model_dir, gold_corpus_file, predicted_corpus_file)

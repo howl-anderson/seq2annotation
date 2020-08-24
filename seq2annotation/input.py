fe3b61ce3bfa0499cbcaf6404bad1a9e92fe1788
Xiaoquan Kong <u1mail2me@gmail.com>
2020-08-24 15:33:23 +0800

    replace generate_tagset with other API

--- seq2annotation/input.py
+++ seq2annotation/input.py
@@ -120,24 +120,25 @@ def build_gold_generator_func(offset_dataset):
     return functools.partial(generator_func, offset_dataset)
 
 
-def generate_tagset(tags) -> List[str]:
-    if not tags:
-        # empty entity still have O tag
-        return [BILUOEncoderDecoder.oscar]
+# def generate_tagset(tags) -> List[str]:
+#     if not tags:
+#         # empty entity still have O tag
+#         return [BILUOEncoderDecoder.oscar]
 
-    tagset = set()
-    for tag in tags:
-        encoder = BILUOEncoderDecoder(tag)
-        tagset.update(encoder.all_tag_set())
+#     tagset = set()
+#     for tag in tags:
+#         encoder = BILUOEncoderDecoder(tag)
+#         tagset.update(encoder.all_tag_set())
 
-    tagset_list = list(tagset)
+#     tagset_list = list(tagset)
 
-    # make sure O is first tag,
-    # this is a bug feature, otherwise sentence_correct is not correct
-    # due to the crf decoder, need fix
-    tagset_list.remove(BILUOEncoderDecoder.oscar)
-    tagset_list = list(sorted(tagset_list, key=lambda x: x))
+#     # make sure O is first tag,
+#     # this is a bug feature, otherwise sentence_correct is not correct
+#     # due to the crf decoder, need fix
+#     tagset_list.remove(BILUOEncoderDecoder.oscar)
+#     tagset_list = list(sorted(tagset_list, key=lambda x: x))
 
-    tagset_list.insert(0, BILUOEncoderDecoder.oscar)
+#     tagset_list.insert(0, BILUOEncoderDecoder.oscar)
 
-    return tagset_list
+#     return tagset_list
+from tokenizer_tools.utils.generate_tagset import generate_tagset
